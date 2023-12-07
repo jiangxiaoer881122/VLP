b27eecf3fab9a3dfaa90f3f07a92db97c8a03ffd
jjl <757405372@qq.com>
2023-12-07 20:04:10 +0800

    this is first pull

---
+++ build/image_preload.cmake
@@ -0,0 +1,8 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf52dk_nrf52832" CACHE INTERNAL "NCS child image controlled")
+set(BOARD_ROOT "d:/nrf52/project/adc;C:/ncs/v2.5.0/nrf;C:/ncs/v2.5.0/zephyr" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "d:/NRF52/project/adc/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "D:/NRF52/project/adc/nrf52dk_nrf52832.overlay" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "C:/ncs/toolchains/c57af46cb7/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
