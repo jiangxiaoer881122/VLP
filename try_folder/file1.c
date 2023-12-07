fcd1f466bddddaed1c0a805f9b5505ab007363bb
jjl <757405372@qq.com>
2023-12-07 17:47:20 +0800

    this  is a test for Vlp_C_project

---
+++ try_folder/file1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+/*用于实验static应用在函数上*/
+static void fun(void)
+{
+    printf("hello from fun.\n");
+}
---
+++ try_folder/file2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/*用于实验static应用在函数上*/
+extern void fun(void);
+int main(void)
+{
+    fun();
+    return 0;
+}
